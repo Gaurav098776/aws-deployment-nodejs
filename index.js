5d61ff9eec5c2b127539051ddc533659758a6756
Gaurav singh lodhi <lodhigaurav23@gmail.com>
2024-08-30 12:43:54 +0530

    new api

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/api/get',(req,res)=>{
      res.send({message: 'Node js AWS deploymnet'})
 })
 
+app.get('/api/user',(req,res)=>{
+  res.send({name:'gaurav'})
+})
+
 
 app.listen(7575,()=>{
   console.log('server is runing on port 7575');
